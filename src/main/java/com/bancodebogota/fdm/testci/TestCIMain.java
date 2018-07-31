d90723630ea7f895ec8bd43da4b686957df0be0a
corre_000 <corre_000@portatil-johny>
2018-07-31 18:01:49 -0500

    primer comit

---
+++ src/main/java/com/bancodebogota/fdm/testci/TestCIMain.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bancodebogota.fdm.testci;
+
+/**
+ *
+ * @author corre_000
+ */
+public class TestCIMain {
+    
+    
+    public int sum(int n1,int n2){
+    
+    return n1+n2;
+    }
+    
+    public static void Main(String[] args){
+    System.err.println("TESTCI");
+    }
+}
